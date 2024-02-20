397fc47f0711bb70d6501dad3ae4087b3a423ea4
Leah Sherley <lsherley90@gmail.com>
2024-02-20 17:41:48 +0300

    Initial commit: Python Basics

---
+++ user_input.py
@@ -0,0 +1,7 @@
+name = input('Enter your name:')
+age = input('Enter your age:')
+location = input('Enter your location:')
+
+print('Thank you for entering your details! Here are the details you entered:')
+print('Hello', name , 'you are', age, 'years old and you live in', location)
+
